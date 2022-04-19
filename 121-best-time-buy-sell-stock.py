0eda122b342d8faa60da13a7e098d5d66e8343bc
mlsimpson <maui@threv.net>
2022-04-19 16:20:17 -0400

    neetcode.io 150 additions

---
+++ 121-best-time-buy-sell-stock.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0 # buy low
+        r = 1 # sell high
+
+        max_profit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                max_profit = max(max_profit, prices[r] - prices[l])
+            else:
+                # in case we find a new minimum further than one index away
+                l = r
+            r += 1
+
+        return max_profit
+
