b024c740b1c1e645c85487529489dc587656f5c4
mlsimpson <maui@threv.net>
2022-04-23 15:39:35 -0400

    reverse linked list notes and refactor

--- 206-reverse-linked-list.py
+++ 206-reverse-linked-list.py
@@ -22,17 +22,20 @@ class Solution:
 # recursive
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # base case: None
+        # hit only if head is empty
         if not head:
             return None
 
         tmp = head
 
+        # reverse the rest of the list
         if head.next:
             tmp = self.reverseList(head.next)
+            # put the first element at the end
             head.next.next = head
+            # if at end, head.next is already None
+            head.next = None
 
-        head.next = None
-
+        # fix the head pointer
         return tmp
 
