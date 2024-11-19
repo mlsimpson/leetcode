513227cb44fcb98067f7cf7836ab33316d8ffcbe
mlsimpson <maui@threv.net>
2024-11-19 16:54:41 -0500

    fixed modes, updated 121

--- 121-best-time-buy-sell-stock.py
+++ 121-best-time-buy-sell-stock.py
@@ -15,3 +15,15 @@ class Solution:
 
         return max_profit
 
+class Solution2:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = prices[0]
+
+        for p in prices:
+            profit = p - min_price
+            max_profit = max(max_profit, profit)
+            min_price = min(p, min_price)
+
+        return max_profit
+
